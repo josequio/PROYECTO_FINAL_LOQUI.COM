6186b318c4f9ee6d62444bc6c178461d2c2bfd82
José Quio <gabrielquio09@gmail.com>
2022-11-10 11:55:28 -0500

    se añade elemento ,change word a las presentación de la pagina con js

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,31 @@ gift_button.addEventListener('click',function(e){
 // --------------------------navegation--------
 const nav_ul = document.querySelector('.nav_ul');
 const nav_button = document.querySelector('.nav_button');
+const nav_access = document.querySelector('.nav_access');
 
 nav_button.addEventListener('click',()=>{
     nav_ul.classList.toggle('activo');
+    nav_access.classList.toggle('activo');
 });
+// ----------chage word-------------------------
+const word = document.querySelector('.presentation_footer_change');
+const words = ['Writing','Speaking','Listening','Una Asesoría Online 🙋‍♂️'];
+
+function change_word(){
+    setTimeout(function(){
+        word.textContent = words[0];
+    },1000);
+    setTimeout(function(){
+        word.textContent = words[1];
+    },2000);
+    setTimeout(function(){
+        word.textContent = words[2];
+    },3000);
+    setTimeout(function(){
+        word.textContent = words[3];
+    },4000);
+
+};
+setInterval(()=>{
+    change_word();
+},1000);
